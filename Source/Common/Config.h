a9cff76f93bc45fc8389840be2ebf22e7b49d39d
Martin Stolz <info@martinstolz.ch>
2022-09-24 19:31:18 +0200

    #6 solved

---
+++ Source/Common/Config.h
@@ -0,0 +1,19 @@
+/**
+ * created by Martin Stolz on 07.03.22
+ *
+ * (c) 2022 by noizplay.com
+ */
+
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace Config {
+
+    // text
+    const auto DEFAULT_TEXT_SIZE{14.f};
+
+    // colours
+    const auto MAIN_COLOUR{Colour(0xFFD9E5B6)};
+
+}
--- Source/Plugin/MidiPluginEditor.cpp
+++ Source/Plugin/MidiPluginEditor.cpp
@@ -4,13 +4,25 @@
  * (c) 2022 by noizplay.com
  */
 
+#include "../Common/CMakeVars.h"
+#include "../Common/Config.h"
+
 #include "MidiPluginEditor.h"
 
 MidiPluginEditor::MidiPluginEditor(AudioProcessor& audioProcessor): AudioProcessorEditor{audioProcessor} {
-    setSize (400, 200);
+    setSize(400, 200);
 }
 
-void MidiPluginEditor::paint (Graphics& graphicsContext) {
-    juce::ignoreUnused(graphicsContext);
+void MidiPluginEditor::paint (Graphics& gContext) {
+    gContext.setColour(Config::MAIN_COLOUR);
+    gContext.setFont(
+            {Config::DEFAULT_TEXT_SIZE, juce::Font::FontStyleFlags::plain }
+    );
+    gContext.drawFittedText(
+            CMakeVars::JUCE_COMPANY_WEBSITE,
+            getLocalBounds(),
+            juce::Justification::bottomLeft,
+            1);
 }
+
 void MidiPluginEditor::resized() {}
