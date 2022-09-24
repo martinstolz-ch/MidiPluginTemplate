bed925047c089e81877c5990e9800f37966babe0
Martin Stolz <info@martinstolz.ch>
2022-09-24 19:31:49 +0200

    #4 solved

---
+++ Source/Common/CMakeVars.h
@@ -0,0 +1,20 @@
+/**
+ * created by Martin Stolz on 07.03.22
+ *
+ * (c) 2022 by noizplay.com
+ */
+
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace CMakeVars {
+
+    #define DECLARE_VAR(name, val) const std::string name{val};
+
+    DECLARE_VAR(JUCE_COMPANY_WEBSITE, "www.noizplay.com")
+    DECLARE_VAR(PROJECT_VERSION, "0.0.1")
+
+    #undef DECLARE_VAR
+
+}
