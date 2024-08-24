b7dd658c15f382dcabd6c8a3bf54a356800451b9
Martin Stolz <info@martinstolz.ch>
2024-08-24 11:59:49 +0200

    initial commit

---
+++ source/etc/cmakeVar.h
@@ -0,0 +1,20 @@
+/**
+ * (c) martin.stolz@noizplay.com
+ */
+
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace cmakeVar {
+
+    static const String
+        buildYear {"2024"},
+
+        companyName {"noizplay"},
+        companyURL {"noizplay.com"},
+
+        projectVersion {"0.0.1"},
+        projectName {"MIDI_PLUGIN"};
+
+}
